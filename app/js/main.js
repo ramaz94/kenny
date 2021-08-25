dcb741df790db0d66cc33d837af816dc98be06c7
ramaz94 <ramaz635@gmail.comm>
2021-08-25 20:32:19 +0300

    add sections

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  $('.header__slider-inner').slick({
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow-prew" type="button"><img src="./img/arrow-prew.svg"></button>',
+    nextArrow: '<button class="slick-arrow-next" type="button"><img src="./img/arrow-next.svg"></button>',
+  });
+
+  
+    
+
+
+
+});
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('sass', function(){
 gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/slick-carousel/slick/slick.css',  
         'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('libs.min.css'))
@@ -33,7 +33,7 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
